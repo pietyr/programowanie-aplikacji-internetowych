1595ca2b859a29a4c51343bebc75b06a4db347f3
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2026-01-17 09:15:01 +0100

    zadanie 6.2 skopiowany kod z laborki

---
+++ lab6/public/funkcje.php
@@ -0,0 +1,161 @@
+<?php
+
+function dodaj($plik)
+{
+    echo "<h3>Dodawanie do pliku:</h3>";
+    walidacja($plik);
+}
+
+/**
+ * Funkcja walidująca dane formularza
+ */
+function walidacja($plik)
+{
+    $args = [
+        'nazwisko' => [
+            'filter' => FILTER_VALIDATE_REGEXP,
+            'options' => [
+                'regexp' => '/^[A-ZĄĆĘŁŃÓŚŹŻ][a-ząćęłńóśźż-]{1,25}$/'
+            ]
+        ],
+        'wiek' => [
+            'filter' => FILTER_VALIDATE_INT,
+            'options' => ['min_range' => 1, 'max_range' => 130]
+        ],
+        'panstwo' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+        'email' => FILTER_VALIDATE_EMAIL,
+        'platnosc' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+        'tutorial' => [
+            'filter' => FILTER_SANITIZE_FULL_SPECIAL_CHARS,
+            'flags' => FILTER_REQUIRE_ARRAY
+        ]
+    ];
+
+    // filtrowanie danych
+    $dane = filter_input_array(INPUT_POST, $args);
+
+    // pokaz wynik walidacji (jak w poleceniu)
+    echo "<pre>";
+    var_dump($dane);
+    echo "</pre>";
+
+    // sprawdzanie błędów
+    $errors = "";
+
+    foreach ($dane as $key => $val) {
+        if ($val === false || $val === null) {
+            $errors .= $key . " ";
+        }
+    }
+
+    if ($errors === "") {
+        dopliku($plik, $dane);
+    } else {
+        echo "<p><strong>Nie poprawne dane:</strong> $errors</p>";
+    }
+}
+
+/**
+ * Zapis danych do pliku
+ */
+function dopliku($nazwaPliku, $tablicaDanych)
+{
+    $linia = "";
+
+    foreach ($tablicaDanych as $value) {
+        if (is_array($value)) {
+            $linia .= implode(",", $value) . " ";
+        } else {
+            $linia .= $value . " ";
+        }
+    }
+
+    $linia .= PHP_EOL;
+
+    file_put_contents($nazwaPliku, $linia, FILE_APPEND);
+
+    echo "<p><strong>Zapisano:</strong><br>$linia</p>";
+}
+
+/**
+ * Wyświetlenie wszystkich danych
+ */
+function pokaz($plik)
+{
+    if (file_exists($plik)) {
+        echo "<h3>Wszystkie zamówienia:</h3>";
+        echo "<pre>";
+        echo htmlspecialchars(file_get_contents($plik));
+        echo "</pre>";
+    }
+}
+
+/**
+ * Filtrowanie po języku
+ */
+function pokaz_zamowienie($jezyk, $plik)
+{
+    if (file_exists($plik)) {
+        $linie = file($plik, FILE_IGNORE_NEW_LINES);
+
+        echo "<h3>Zamówienia na tutorial: $jezyk</h3>";
+        echo "<pre>";
+
+        foreach ($linie as $linia) {
+            if (stripos($linia, $jezyk) !== false) {
+                echo htmlspecialchars($linia) . PHP_EOL;
+            }
+        }
+
+        echo "</pre>";
+    }
+}
+
+/**
+ * Statystyki zamówień
+ */
+function statystyki($plik)
+{
+    if (!file_exists($plik)) {
+        echo "<p>Brak danych do statystyk.</p>";
+        return;
+    }
+
+    $linie = file($plik, FILE_IGNORE_NEW_LINES);
+
+    $wszystkie = 0;
+    $ponizej18 = 0;
+    $powyzej49 = 0;
+
+    foreach ($linie as $linia) {
+        if (trim($linia) === "") continue;
+
+        $wszystkie++;
+
+        $dane = explode(" ", $linia);
+        $wiek = (int)$dane[1]; // wiek jest drugim polem
+
+        if ($wiek < 18) {
+            $ponizej18++;
+        }
+
+        if ($wiek >= 50) {
+            $powyzej49++;
+        }
+    }
+
+    echo "<h3>Statystyki zamówień:</h3>";
+    echo "<p>Liczba wszystkich zamówień: $wszystkie</p>";
+    echo "<p>Liczba zamówień od osób w wieku &lt; 18 lat: $ponizej18</p>";
+    echo "<p>Liczba zamówień od osób w wieku ≥ 50 lat: $powyzej49</p>";
+}
+
+function dodajdoBD($bd)
+{
+//pobierz dane z formularza, dokonaj ich walidacji
+//jeśli dane są poprawne - sformułuj zapytanie $sql typu insert np.:
+// INSERT INTO klienci VALUES (NULL, 'Babacka', '22', 'Niemcy',
+// 'bbbp@ollub.pl', 'Java,CPP', 'Przelew');
+//i wywołaj metodę:
+//$bd->insert($sql);
+}
---
+++ lab6/public/index.php
@@ -0,0 +1,21 @@
+<?php
+include 'funkcje.php';
+include_once '../Baza.php';
+
+$bd = new Baza('localhost',)
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Klienci</title>
+</head>
+<body>
+<h1>Laboratorium 6</h1>
+<h2>Klienci baza</h2>
+</body>
+</html>
